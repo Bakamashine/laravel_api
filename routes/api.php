aeda73a39b194e86a0435aaeb3b4011f941b94ff
Bakamashine <surajaaminu09@gmail.com>
2025-04-28 21:59:53 +0300

    Небольшие правки

--- routes/api.php
+++ routes/api.php
@@ -7,24 +7,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\ApiHelper;
-// use Laravel\Fortify\Http\Controllers\RegisteredUserController;
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 
 // Получение информации о пользователе
 Route::get("/users/getinfo", function (Request $request) {
     return auth('sanctum')->user();
 })->middleware("auth:sanctum");
 
-
-
-Route::get("test", function () {
-    return 2;
-})->middleware("auth:sanctum");
 
 // Администратор
 Route::middleware(['auth:sanctum', 'abilities:1'])
